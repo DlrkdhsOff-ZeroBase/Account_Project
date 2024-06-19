4714f3aceb057f79d994aee8a53d0dcdaa26b9c4
dlrkdhsoff <dlrkdhsoff@gmail.com>
2024-06-19 22:34:42 +0900

    successAccountTest

---
+++ src/main/java/com/example/account/domain/AccountUser.java
@@ -0,0 +1,30 @@
+package com.example.account.domain;
+
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class AccountUser {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @CreatedDate
+    private LocalDate createdAt;
+    @LastModifiedDate
+    private LocalDate updatedAt;
+}
