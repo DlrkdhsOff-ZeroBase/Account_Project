03a6133be642e2997ebeed75593a44302d785a96
Lee Kaon <dlrkdhsoff@gmail.com>
2024-06-21 16:27:37 +0900

    create AccountInfo, update AccountController, AccountService, AccountRepository

--- src/main/java/com/example/account/repository/AccountRepository.java
+++ src/main/java/com/example/account/repository/AccountRepository.java
@@ -4,6 +4,7 @@ import com.example.account.domain.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +14,6 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     Integer countByAccountUser(AccountUser accountUser);
 
     Optional<Account> findByAccountNumber(String AccountNumber);
+
+    List<Account> findByAccountUser(AccountUser accountUser);
 }
