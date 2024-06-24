3b7a25d6978feb211f65bc88eb7165243591c3ce
Lee Kaon <dlrkdhsoff@gmail.com>
2024-06-24 17:24:15 +0900

    update AccountRepository

--- src/main/java/com/example/account/repository/AccountRepository.java
+++ src/main/java/com/example/account/repository/AccountRepository.java
@@ -9,8 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    Optional<Account> findFirstByOrderByIdDesc();
-
     boolean existsByAccountNumber(String accountNumber);
 
     Integer countByAccountUser(AccountUser accountUser);
